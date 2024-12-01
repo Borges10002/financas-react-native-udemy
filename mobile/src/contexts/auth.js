392c4bc3d1625454b745e38f7fbe58fa4844b293
Borges10002 <borges10002@gmail.com>
2024-12-01 18:14:49 -0400

    auth

--- mobile/src/contexts/auth.js
+++ mobile/src/contexts/auth.js
@@ -1,29 +1,59 @@
-import {useNavigation} from '@react-navigation/native';
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
+
 import api from '../services/api';
+import {useNavigation} from '@react-navigation/native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const AuthContext = createContext({});
 
 function AuthProvider({children}) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const navigation = useNavigation();
 
+  useEffect(() => {
+    async function loadStorage() {
+      const storageUser = await AsyncStorage.getItem('@finToken');
+
+      if (storageUser) {
+        const response = await api
+          .get('/me', {
+            headers: {
+              Authorization: `Bearer ${storageUser}`,
+            },
+          })
+          .catch(() => {
+            setUser(null);
+          });
+
+        api.defaults.headers.Authorization = `Bearer ${storageUser}`;
+        setUser(response.data);
+        setLoading(false);
+      }
+
+      setLoading(false);
+    }
+
+    loadStorage();
+  }, []);
+
   async function signUp(email, password, nome) {
     setLoadingAuth(true);
+
     try {
       const response = await api.post('/users', {
         name: nome,
         password: password,
         email: email,
       });
-
       setLoadingAuth(false);
 
       navigation.goBack();
-    } catch (error) {
-      console.log('ERRO AO CADASTRAR', error);
+    } catch (err) {
+      console.log('ERRO AO CADASTRAR', err);
       setLoadingAuth(false);
     }
   }
@@ -46,20 +76,26 @@ function AuthProvider({children}) {
         email,
       };
 
+      await AsyncStorage.setItem('@finToken', token);
+
       api.defaults.headers.Authorization = `Bearer ${token}`;
 
-      setUser({id, name, email});
+      setUser({
+        id,
+        name,
+        email,
+      });
 
       setLoadingAuth(false);
-    } catch (error) {
-      console.log('ERRO AO LOGAR', error);
+    } catch (err) {
+      console.log('ERRO AO LOGAR ', err);
       setLoadingAuth(false);
     }
   }
 
   return (
     <AuthContext.Provider
-      value={{signed: !!user, user, signUp, signIn, loadingAuth}}>
+      value={{signed: !!user, user, signUp, signIn, loadingAuth, loading}}>
       {children}
     </AuthContext.Provider>
   );
