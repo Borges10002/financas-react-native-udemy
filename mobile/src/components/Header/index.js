75cda364f742c9eef2a503beaab220dc6d7c0b58
Borges10002 <borges10002@gmail.com>
2024-12-29 14:59:36 -0400

    update

--- mobile/src/components/Header/index.js
+++ mobile/src/components/Header/index.js
@@ -1,11 +1,16 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
+
 import Icon from 'react-native-vector-icons/Feather';
-import {Container, Title, ButtonMenu} from './styles';
+
+import {ButtonMenu, Container, Title} from './styles';
 
 export default function Header({title}) {
+  const navigation = useNavigation();
+
   return (
     <Container>
-      <ButtonMenu>
+      <ButtonMenu onPress={() => navigation.openDrawer()}>
         <Icon name="menu" size={35} color="#121212" />
       </ButtonMenu>
       {title && <Title>{title}</Title>}
