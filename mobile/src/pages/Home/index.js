00208b9d40cddef5aa6b5935d89d9fc9e2e4bfa2
Borges10002 <borges10002@gmail.com>
2024-12-29 22:15:14 -0400

    update

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import Header from '../../components/Header';
-import {Background} from './sytles';
+import {Background} from './styles';
+
+import {format} from 'date-fns';
+import api from '../../services/api';
 
 export default function Home() {
+  const [listBalance, setListBalance] = useState([]);
+
+  const [dateMovements, setDateMovements] = useState(new Date());
+
+  useEffect(() => {
+    let isActive = true;
+
+    async function getMovements() {
+      let dateFormated = format(dateMovements, 'dd/MM/yyyy');
+
+      const balance = await api.get('/balance', {
+        params: {
+          date: dateFormated,
+        },
+      });
+
+      if (isActive) {
+        setListBalance(balance.data);
+      }
+    }
+
+    getMovements();
+
+    return () => (isActive = false);
+  }, [dateMovements]);
+
   return (
     <Background>
       <Header title="Minhas movimentações" />
