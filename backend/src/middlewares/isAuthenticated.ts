5f79b55fd814b9929c93893686b2ae717481a31f
Borges10002 <borges10002@gmail.com>
2025-01-02 22:35:27 -0400

    update

--- backend/src/middlewares/isAuthenticated.ts
+++ backend/src/middlewares/isAuthenticated.ts
@@ -1,35 +1,33 @@
-import {Request, Response, NextFunction } from 'express'
-import { verify } from 'jsonwebtoken'
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
 
-interface Payload{
+interface Payload {
   sub: string;
 }
 
 export function isAuthenticated(
   request: Request,
   response: Response,
-  next: NextFunction,
-){
+  next: NextFunction
+) {
   const authToken = request.headers.authorization;
 
-  if(!authToken){
+  if (!authToken) {
     return response.status(401).end();
   }
 
   const [, token] = authToken.split(" ");
 
-  try{
+  try {
     const { sub } = verify(
       token,
-      "4f93ac9d10cb751b8c9c646bc9dbccb9",
+      "4f93ac9d10cb751b8c9c646bc9dbccb9"
     ) as Payload;
 
     request.user_id = sub;
 
     return next();
-
-  }catch(err){
+  } catch (err) {
     return response.status(401).end();
   }
-
 }
