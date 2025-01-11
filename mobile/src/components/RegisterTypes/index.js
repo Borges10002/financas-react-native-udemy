7250586bf8efb9da9a88843eaeaf793722a8e522
Borges10002 <borges10002@gmail.com>
2025-01-10 20:05:30 -0400

    list of gastos

--- mobile/src/components/RegisterTypes/index.js
+++ mobile/src/components/RegisterTypes/index.js
@@ -1,36 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Feather from 'react-native-vector-icons/Feather';
 
-import {RegisterContainer, RegisterTypeButton, RegisterLabel} from './styles';
+import { RegisterContainer, RegisterTypeButton, RegisterLabel } from './styles';
 
-export default function RegisterTypes({type, sendTypeChanged}) {
-  const [typeChecked, setTypeChecked] = useState(type);
+export default function RegisterTypes({ type, sendTypeChanged }){
+  const [typeChecked, setTypeChecked] = useState(type)
 
-  function changeType(name) {
-    if (name === 'receita') {
+  function changeType(name){
+    if(name === 'receita'){
       setTypeChecked('receita');
       sendTypeChanged('receita');
-    } else {
+    }else{
       setTypeChecked('despesa');
       sendTypeChanged('despesa');
     }
   }
 
-  return (
+  return(
     <RegisterContainer>
-      <RegisterTypeButton
-        checked={typeChecked === 'receita' ? true : false}
-        onPress={() => changeType('receita')}>
-        <Feather name="arrow-up" size={25} color="#121212" />
-        <RegisterLabel>Receita</RegisterLabel>
+      <RegisterTypeButton 
+        checked={ typeChecked === 'receita' ? true : false} 
+        onPress={ () => changeType('receita') } 
+      >
+        <Feather name="arrow-up" size={25} color="#121212"/>
+        <RegisterLabel>
+          Receita
+        </RegisterLabel>
       </RegisterTypeButton>
 
-      <RegisterTypeButton
-        checked={typeChecked === 'despesa' ? true : false}
-        onPress={() => changeType('despesa')}>
-        <Feather name="arrow-down" size={25} color="#121212" />
-        <RegisterLabel>Despesa</RegisterLabel>
+      <RegisterTypeButton 
+      checked={ typeChecked === 'despesa' ? true : false}
+      onPress={ () => changeType('despesa') } 
+      >
+        <Feather name="arrow-down" size={25} color="#121212"/>
+        <RegisterLabel>
+          Despesa
+        </RegisterLabel>
       </RegisterTypeButton>
+
     </RegisterContainer>
-  );
+  )
 }
--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,52 +1,49 @@
-import React, { useContext, useEffect, useState } from 'react';
-import {View, Text, Button} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {TouchableOpacity} from 'react-native';
 
-import { AuthContext } from '../../contexts/auth'
+import {AuthContext} from '../../contexts/auth';
 
 import Header from '../../components/Header';
-import { 
-  Background, 
-  ListBalance
- } from './styles'; 
+import {Background, ListBalance, Area, Title, List} from './styles';
 
-import api from '../../services/api'
-import { format } from 'date-fns';
+import api from '../../services/api';
+import {format} from 'date-fns';
 
-import { useIsFocused } from '@react-navigation/native';
+import {useIsFocused} from '@react-navigation/native';
 import BalanceItem from '../../components/BalanceItem';
+import HistoricoList from '../../components/HistoricoList';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Home(){
+export default function Home() {
   const isFocused = useIsFocused();
   const [listBalance, setListBalance] = useState([]);
 
-  const [dateMovements, setDateMovements] = useState(new Date())
+  const [dateMovements, setDateMovements] = useState(new Date());
 
-
-  useEffect(()=>{
+  useEffect(() => {
     let isActive = true;
 
-    async function getMovements(){
+    async function getMovements() {
       let dateFormated = format(dateMovements, 'dd/MM/yyyy');
 
       const balance = await api.get('/balance', {
-        params:{
-          date: dateFormated 
-        }
-      })
+        params: {
+          date: dateFormated,
+        },
+      });
 
-      if(isActive){
+      if (isActive) {
         setListBalance(balance.data);
       }
     }
 
     getMovements();
 
-    return () => isActive = false;
+    return () => (isActive = false);
+  }, [isFocused]);
 
-  }, [isFocused])
-
-  return(
+  return (
     <Background>
       <Header title="Minhas movimentações" />
 
@@ -54,10 +51,23 @@ export default function Home(){
         data={listBalance}
         horizontal={true}
         showsHorizontalScrollIndicator={false}
-        keyExtractor={ item => item.tag }
-        renderItem={ ({ item }) => ( <BalanceItem data={item} /> )}
+        keyExtractor={item => item.tag}
+        renderItem={({item}) => <BalanceItem data={item} />}
       />
 
+      <Area>
+        <TouchableOpacity>
+          <Icon name="event" color="#121212" size={30} />
+        </TouchableOpacity>
+        <Title>Ultimas movimentações</Title>
+      </Area>
+
+      <List
+        data={[]}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <HistoricoList />}
+        showsVerticalScrollIndicator={false}
+      />
     </Background>
-  )
+  );
 }
