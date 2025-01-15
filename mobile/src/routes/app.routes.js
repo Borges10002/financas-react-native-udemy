487076a6471db4f7f93952758109e0e090086424
Borges10002 <borges10002@gmail.com>
2025-01-14 21:23:26 -0400

    pagina perfil

--- mobile/src/routes/app.routes.js
+++ mobile/src/routes/app.routes.js
@@ -1,41 +1,36 @@
 import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import Home from '../pages/Home';
-import New from '../pages/New'
+import New from '../pages/New';
+import Profile from '../pages/Profile';
 
 const AppDrawer = createDrawerNavigator();
 
-function AppRoutes(){
-  return(
+function AppRoutes() {
+  return (
     <AppDrawer.Navigator
       screenOptions={{
-        headerShown:false,
+        headerShown: false,
 
-        drawerStyle:{
+        drawerStyle: {
           backgroundColor: '#FFF',
           paddingTop: 20,
         },
 
-        drawerActiveBackgroundColor:'#3b3dbf',
+        drawerActiveBackgroundColor: '#3b3dbf',
         drawerActiveTintColor: '#FFF',
 
         drawerInactiveBackgroundColor: '#F0F2FF',
-        drawerInactiveTintColor: '#121212'
+        drawerInactiveTintColor: '#121212',
+      }}>
+      <AppDrawer.Screen name="Home" component={Home} />
 
-      }}
-    >
-      <AppDrawer.Screen
-        name="Home"
-        component={Home}
-      />
+      <AppDrawer.Screen name="Registrar" component={New} />
 
-      <AppDrawer.Screen
-        name="Registrar"
-        component={New}
-      />
+      <AppDrawer.Screen name="Perfil" component={Profile} />
     </AppDrawer.Navigator>
-  )
+  );
 }
 
 export default AppRoutes;
