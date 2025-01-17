596ad2840a2e19178d787bed0ec6fdf631b934d6
Borges10002 <borges10002@gmail.com>
2025-01-16 22:06:15 -0400

    personal register

--- mobile/src/routes/app.routes.js
+++ mobile/src/routes/app.routes.js
@@ -1,36 +1,50 @@
 import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import Home from '../pages/Home';
 import New from '../pages/New';
 import Profile from '../pages/Profile';
 
+import CustomDrawer from '../components/CustomDrawer';
+
 const AppDrawer = createDrawerNavigator();
 
-function AppRoutes() {
-  return (
+function AppRoutes(){
+  return(
     <AppDrawer.Navigator
+      drawerContent={ (props) => <CustomDrawer {...props} /> }
       screenOptions={{
-        headerShown: false,
+        headerShown:false,
 
-        drawerStyle: {
+        drawerStyle:{
           backgroundColor: '#FFF',
           paddingTop: 20,
         },
 
-        drawerActiveBackgroundColor: '#3b3dbf',
+        drawerActiveBackgroundColor:'#3b3dbf',
         drawerActiveTintColor: '#FFF',
 
         drawerInactiveBackgroundColor: '#F0F2FF',
-        drawerInactiveTintColor: '#121212',
-      }}>
-      <AppDrawer.Screen name="Home" component={Home} />
+        drawerInactiveTintColor: '#121212'
 
-      <AppDrawer.Screen name="Registrar" component={New} />
+      }}
+    >
+      <AppDrawer.Screen
+        name="Home"
+        component={Home}
+      />
 
-      <AppDrawer.Screen name="Perfil" component={Profile} />
+      <AppDrawer.Screen
+        name="Registrar"
+        component={New}
+      />
+
+      <AppDrawer.Screen
+        name="Perfil"
+        component={Profile}
+      />
     </AppDrawer.Navigator>
-  );
+  )
 }
 
 export default AppRoutes;
